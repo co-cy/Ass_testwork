d784258b6c1a365e401345d57fe1b0bf3ee8cf09
_co_cy_ <stf2003.kem@gmail.com>
2022-11-03 20:52:38 +0300

    [develop] -> Added a search engine

--- server/routes/index.py
+++ server/routes/index.py
@@ -1,5 +1,11 @@
+from server.elastic import get_elastic, AsyncElasticsearch
+from server.database import get_session, AsyncSession
+from server.database.tables.document import Document
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
-from fastapi import APIRouter
+from typing import Iterator
+from asyncio import gather
+import json
 
 router = APIRouter()
 
@@ -9,5 +15,16 @@ class SearchString(BaseModel):
 
 
 @router.post("/")
-def index(search_string: SearchString):
-    return search_string.string
+async def index(search_string: SearchString, db_session: AsyncSession = Depends(get_session),
+                elastic: AsyncElasticsearch = Depends(get_elastic)):
+    result = await elastic.search(index="document", query={"match": {"text": search_string.string}}, size=10)
+
+    list_hits = list(map(lambda hit: int(hit["_id"]), result["hits"]["hits"]))
+    list_doc = []
+    for i in list_hits:
+        list_doc.append(db_session.get(Document, i))
+
+    doc_result: Iterator[Document] = sorted(await gather(*list_doc), key=lambda x: x.created_date, reverse=True)
+    for doc in doc_result:
+        doc.rubrics = json.loads(doc.rubrics)
+    return doc_result
